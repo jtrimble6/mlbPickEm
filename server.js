e8d1af99e994ca6e7847db41187a4120bf32f2ee
JOSHUA TRIMBLE <JAYTRIM6@GMAIL.COM>
2019-02-20 22:13:18 -0600

    going live

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 //   app.use(express.static(path.join(__dirname, "client", "build")));
 // }
 
-app.use('/mlbpickem/', express.static(path.join(__dirname, "client", "build")));
+app.use('/mlbpickem/', express.static(path.join(__dirname, "client/build")));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -70,7 +70,7 @@ app.use( (req, res, next) => {
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mlbpickem");
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 // Start the API server
