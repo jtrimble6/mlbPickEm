0924b4786ddebf2b36101fad0179ac17f41e61f2
JOSHUA TRIMBLE <JAYTRIM6@GMAIL.COM>
2019-03-22 11:14:08 -0500

    working on logout

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mlbPickEm/precache-manifest.26db48767fe68c66366f7744146aa853.js"
+  "/mlbPickEm/precache-manifest.8edd2e8449ec9c8edc531482f6fd3a14.js"
 );
 
 workbox.clientsClaim();
--- client/src/pages/landings/LogoutPage.jsx
+++ client/src/pages/landings/LogoutPage.jsx
@@ -19,27 +19,27 @@ class LogoutPage extends Component {
 
     renderLogout = () => {
         localStorage.clear()
-        // let afterLogoutUser = localStorage.getItem('user')
-        // // console.log('USER AFTER LOGOUT: ', afterLogoutUser)
-        // if (afterLogoutUser === null) {
-        //     this.setState({
-        //         loggedOut: true
-        //     })
-        //   }
+        let afterLogoutUser = localStorage.getItem('user')
+        // console.log('USER AFTER LOGOUT: ', afterLogoutUser)
+        if (afterLogoutUser === null) {
+            this.setState({
+                loggedOut: true
+            })
+          }
     }
     
 
 
     render() {
         // let background1 = require('../../css/images/landing2.jpeg')
-        // let loggedOut = this.state.loggedOut
+        let loggedOut = this.state.loggedOut
         return (
             <div id='logoutPage'>
               <h2>Thank you for doing your chores!</h2>
-                {/* {
+                {
                   (loggedOut) ? <small id="logoutSuccess" className="form-text text-muted">You have successfully logged out!</small> :
                   <small id="logoutFailure" className="form-text text-muted">There was an error logging you out!</small>
-                } */}
+                }
             </div>
         )
     }
