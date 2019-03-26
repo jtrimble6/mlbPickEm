dc4b9786f35b5d42394221a045165a31d61c58cf
JOSHUA TRIMBLE <JAYTRIM6@GMAIL.COM>
2019-03-26 13:29:15 -0500

    fixed bug

--- routes/API/userAPI/index.js
+++ routes/API/userAPI/index.js
@@ -4,6 +4,5 @@ const userRoutes = require("./user");
 router.use("/api/users", userRoutes)
 router.use("/api/users/login", userRoutes)
 router.use("/user", userRoutes)
-router.use("/logout", userRoutes)
 
 module.exports = router;
