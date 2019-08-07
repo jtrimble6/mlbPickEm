945f720ca1559e74b3aace7c3d39b12225ba4eb1
JOSHUA TRIMBLE <JAYTRIM6@GMAIL.COM>
2019-08-07 12:07:21 -0500

    readding teams

--- controllers/nflTeamController.js
+++ controllers/nflTeamController.js
@@ -24,7 +24,7 @@ module.exports = {
           db.NflTeam
             .update(
                 { teamAlias: req.params.abbr },
-                { $push: { homeGames: req.body }})
+                { $push: { awayGames: req.body }})
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err))
       },
