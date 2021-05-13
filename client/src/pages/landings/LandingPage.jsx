325658bfe33f8a63480462a25318b132e10ef19e
JOSHUA TRIMBLE <JAYTRIM6@GMAIL.COM>
2021-05-13 09:47:19 -0500

    fixing site

--- client/src/pages/landings/LandingPage.jsx
+++ client/src/pages/landings/LandingPage.jsx
@@ -26,7 +26,8 @@ class LandingPage extends Component {
 
     handlePreloader() {
       console.log('HANDLE PRELOADER')
-      $(".se-pre-con").fadeOut("slow");
+      // $(".se-pre-con").fadeOut("slow");
+      $(".se-pre-con").hide();
     }
 
     setRedirect = () => {
